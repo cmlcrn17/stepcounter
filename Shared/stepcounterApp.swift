949ba58caae9d7917a912a58e774ba77fc60f087
Ceren <ctassin@geobilgi.com>
2022-01-09 20:10:29 +0300

    Initial Commit

---
+++ Shared/stepcounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepcounterApp.swift
+//  Shared
+//
+//  Created by Ceren TAŞSIN on 9.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct stepcounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
